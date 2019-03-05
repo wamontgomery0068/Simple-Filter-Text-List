4321934e2458c8597eafc27e05b26222a305c806
Your Name <wamontgomery0068@gmail.com>
2019-03-05 14:20:24 -0600

    Added Baseball Team Names

--- src/App.js
+++ src/App.js
@@ -2,6 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      filterString: "",
+      americanLeague: [
+        "Baltimore Orioles",
+        "Boston Red Sox",
+        "Chicago White Sox",
+        "Cleveland Indians",
+        "Detroit Tigers",
+        "Houston Astros",
+        "Kansas City Royals",
+        "Los Angeles Angels",
+        "Minnesota Twins",
+        "New York Yankees",
+        "Oakland Athletics",
+        "Seattle Mariners",
+        "Tampa Bay Rays",
+        "Texas Rangers",
+        "Toronto Blue Jays",
+      ],
+      nationalLeague: [
+        "Arizona Diamondbacks",
+        "Atlanta Braves",
+        "Chicago Cubs",
+        "Cincinnati Reds",
+        "Colorado Rockies",
+        "Miami Marlins",
+        "Milwaukee Brewers",
+        "New York Mets",
+        "Philadelphia Phillies",
+        "Pittsburgh Pirates",
+        "San Diego Padres",
+        "San Francisco Giants",
+        "St. Louis Cardinals",
+        "Washington Nationals",
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
